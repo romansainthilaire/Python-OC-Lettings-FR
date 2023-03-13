4d111eaa7adb96a883cced53f14b34442253dfbe
Roman Saint-Hilaire <sainthilaireroman@gmail.com>
2023-03-13 13:23:11 +0100

    test index

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,2 +1,9 @@
-def test_dummy():
-    assert 1
+from django.urls import reverse
+
+
+def test_index(client):
+    path = reverse("index")
+    response = client.get(path)
+    content = response.content.decode()
+    assert response.status_code == 200
+    assert "<title>Holiday Homes</title>" in content
