4f37c75fe904cb36d2c9a86f6ce5ebd717ce589a
Roman Saint-Hilaire <sainthilaireroman@gmail.com>
2023-03-13 12:26:35 +0100

    rafactoring

--- lettings/admin.py
+++ lettings/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from lettings.models import Letting, Address
+from lettings.models import Address, Letting
 
 
-admin.site.register(Letting)
 admin.site.register(Address)
+admin.site.register(Letting)
