9d58984c62642611c66f0e8c2aa36c1d84481018
Roman Saint-Hilaire <sainthilaireroman@gmail.com>
2023-03-15 10:21:50 +0100

    createadmin command

---
+++ profiles/management/commands/createadmin.py
@@ -0,0 +1,18 @@
+import os
+
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from django.db.utils import IntegrityError
+
+
+class Command(BaseCommand):
+    def handle(self, **options):
+        User = get_user_model()
+        try:
+            User.objects.create_superuser(
+                os.environ.get("ADMIN_USERNAME"),
+                os.environ.get("ADMIN_EMAIL"),
+                os.environ.get("ADMIN_PASSWORD")
+                )
+        except IntegrityError:
+            pass
