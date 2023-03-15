c155174cd00b7e5668d422b414d484efbd0c0f5a
Roman Saint-Hilaire <sainthilaireroman@gmail.com>
2023-03-15 14:27:24 +0100

    test

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -2,7 +2,7 @@ import os
 
 import django_heroku
 
-# this is a testttttt
+# this is a test
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
