f9d6b1b84d042e75802a76e184a451d7411720b4
Roman Saint-Hilaire <sainthilaireroman@gmail.com>
2023-03-16 16:44:31 +0100

    using sentry

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,6 +1,7 @@
 import os
 
 import django_heroku
+import sentry_sdk
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -106,13 +107,32 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# Deployment with Heroku
+# Heroku
 
 django_heroku.settings(locals())
+
+
+# Sentry
+
+sentry_sdk.init(
+    dsn=os.environ.get("SENTRY_DSN"),
+    integrations=[
+        sentry_sdk.integration.django.DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
