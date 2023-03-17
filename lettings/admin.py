303f2fe6ba797bc80c47bc92ee710f89477876e9
Roman Saint-Hilaire <sainthilaireroman@gmail.com>
2023-03-17 16:24:31 +0100

    customize admin site

--- lettings/admin.py
+++ lettings/admin.py
@@ -3,5 +3,26 @@ from django.contrib import admin
 from lettings.models import Address, Letting
 
 
-admin.site.register(Address)
-admin.site.register(Letting)
+class AdressAdmin(admin.ModelAdmin):
+
+    list_display = ["address", "country_iso_code"]
+    list_filter = ["city"]
+
+    def address(self, obj):
+        return f"{obj.number} {obj.street}, {obj.city}, {obj.state} {obj.zip_code}"
+
+
+class LettingAdmin(admin.ModelAdmin):
+
+    list_display = ["title", "location"]
+    search_fields = ["title"]
+
+    def location(self, obj):
+        return (
+            f"{obj.address.number} {obj.address.street}, {obj.address.city}, " +
+            f"{obj.address.state} {obj.address.zip_code}"
+            )
+
+
+admin.site.register(Address, AdressAdmin)
+admin.site.register(Letting, LettingAdmin)
--- profiles/admin.py
+++ profiles/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 from profiles.models import Profile
 
 
-admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+
+    list_display = ["user", "favorite_city"]
+    list_filter = ["favorite_city"]
+
+
+admin.site.register(Profile, ProfileAdmin)
+
+admin.site.site_header = "Orange County Lettings"
